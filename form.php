a5a61ab3a7a8f02989ac6d08a7c40e90f2600539
MarTz-XII <113044115+MarTz-XII@users.noreply.github.com>
2023-03-14 22:01:46 -0600

    Update form.php

--- form.php
+++ form.php
@@ -7,15 +7,3 @@ $Apellido = $_POST ['apellido'];
 
 
 $mensaje = "Este mensaje fue enviado por " . $nombre . "\r\n";
-$mensaje = "Enviado la fecha del " . date('d/m/Y',time());
-
-$para = 'martzrl4@gmail.com';
-$asunto = 'Este mail fue enviado desde la pagina web';
-
-
-mail($para, $asunto, $nombre);
-echo "<script>alert('correo enviado exitosamente') </script>";
-echo "<script> setTimeout(\"location.href='index.html'\",2000) </script>";
-
-
-?>
